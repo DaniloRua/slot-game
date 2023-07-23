c8fff544195503e1babbb32a3895e5f7a8e68ebf
Danilopc <sr_danilo@hotmail.com>
2023-07-23 20:12:52 +0100

    js + new layout

---
+++ script.js
@@ -0,0 +1,47 @@
+const msg = document.getElementById("message");
+
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 2) + 1;
+    }
+
+    function getElement(id) {
+        return document.getElementById(id);
+    }
+
+    function playWonSound() {
+        const wonSound = document.getElementById("won_sound");
+        wonSound.play();
+    }
+    function showMessage() {
+
+        msg.style.display = "flex"
+
+    }
+    function hideMessage() {
+
+        msg.style.display = "none"
+    }
+
+    function spin() {
+
+        const number1 = getElement("number1")
+        const number2 = getElement("number2")
+        const number3 = getElement("number3")
+
+        const num1 = getRandomNumber();
+        const num2 = getRandomNumber();
+        const num3 = getRandomNumber();
+
+        number1.innerHTML = `${num1}`
+        number2.innerHTML = `${num2}`
+        number3.innerHTML = `${num3}`
+
+
+        if (num1 === num2 && num1 === num3) {
+            playWonSound();
+            showMessage();
+        } else {
+            hideMessage();
+        }
+    }
