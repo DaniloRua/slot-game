73740e1bbed01417f83ae07c15907ac55d5140b6
Danilopc <sr_danilo@hotmail.com>
2023-07-30 23:46:42 +0100

    slots roller done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const msg = document.getElementById("message");
-
+const startBtn = document.getElementById("button");
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 9) + 1;
+    return Math.floor(Math.random() * 2) + 1;
 }
 
 function getElement(id) {
@@ -25,33 +25,67 @@ function hideMessage() {
 
 function result() {
 
+    const number1 = document.getElementById("number1").value;
+    const number2 = document.getElementById("number2").value;
+    const number3 = document.getElementById("number3").value;
+    
+    
 
-    const number1 = getElement("number1")
-    const number2 = getElement("number2")
-    const number3 = getElement("number3")
+  
+         console.log(number1.value,number2,number3)
+    // if (number1 === number2 && number1 === number3) {
+    //     playWonSound();
+    //     showMessage();
+    // } else {
+    //     hideMessage();
+    // }
+}
+
+
+function animation() {
+
+
+  const  rolls = ()=> { 
+   
+    const number1 = getElement("number1");
+    const number2 = getElement("number2");
+    const number3 = getElement("number3");
 
     const num1 = getRandomNumber();
     const num2 = getRandomNumber();
     const num3 = getRandomNumber();
 
-    number1.innerHTML = `${num1}`
-    number2.innerHTML = `${num2}`
-    number3.innerHTML = `${num3}`
+    number1.innerHTML = `${num1}`;
+    number2.innerHTML = `${num2}`;
+    number3.innerHTML = `${num3}`;
+   
+
+   }
+
+   const interval = setInterval(
+    rolls
+  ,80)
+   
+
+  const timeOut = setTimeout(()=>{
+
+    clearInterval(interval);
+
+    result()
+
+   },1000)
 
 
-    if (num1 === num2 && num1 === num3) {
-        playWonSound();
-        showMessage();
-    } else {
-        hideMessage();
-    }
 }
 
-    function roller () {
 
 
 
-        setTimeout(result,2000)
-    }
+function roller() {
+
+    animation();
+   
+
+}
 
 
